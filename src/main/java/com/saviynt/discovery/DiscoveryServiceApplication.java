8cf027c48a6041cc636d1d9cb2e0abf56d32f386
Siva <sendtosankar@gmail.com>
2025-09-01 08:34:12 +0530

    railway changes

--- src/main/java/com/saviynt/discovery/DiscoveryServiceApplication.java
+++ src/main/java/com/saviynt/discovery/DiscoveryServiceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class,
+    org.springframework.boot.actuate.autoconfigure.metrics.JvmMetricsAutoConfiguration.class
+})
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
